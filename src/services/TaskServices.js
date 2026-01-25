9d1161918f00fa10624a95518a59f7e74d700e7a
Krish Shah <kshah@trueigtech.com>
2026-01-26 00:58:00 +0530

    Refactored: removed harcoded link

--- src/services/TaskServices.js
+++ src/services/TaskServices.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const createTask = async (taskData) => {
-  const response = await axios.post("http://localhost:3000/tasks", taskData);
+  const response = await axios.post(API_URL, taskData);
   if (response.status !== 201) throw Error("Failed to create task");
   return response.data;
 };
 export const fetchTasks = async () => {
-  const response = await axios.get("http://localhost:3000/tasks");
+  const response = await axios.get(API_URL);
   if (response.status !== 200) throw Error("Failed to fetch tasks");
   return response.data;
 };
@@ -19,7 +21,7 @@ export const updateTask = async ({
   dueDate,
   assignee,
 }) => {
-  const response = await axios.patch(`http://localhost:3000/tasks/${id}`, {
+  const response = await axios.patch(`${API_URL}/${id}`, {
     title: title,
     status: status,
     priority: priority,
@@ -32,7 +34,7 @@ export const updateTask = async ({
 };
 
 export const deleteTask = async ({ id }) => {
-  const response = await axios.delete(`http://localhost:3000/tasks/${id}`);
+  const response = await axios.delete(`${API_URL}/${id}`);
   if (response.status !== 200) throw Error("Failed to Delete task");
   return response.status;
 };
