fbd27aca3d6f476d0bf254e4ffe24c95356f4c8e
Krish Shah <kshah@trueigtech.com>
2026-01-25 21:27:55 +0530

    CRUD operations of Tasks

---
+++ src/services/TaskServices.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const createTask = async ({ title, priority, dueDate }) => {
+  const response = await axios.post("http://localhost:3000/tasks", {
+    id: Date.now(),
+    title: title,
+    status: "to-do",
+    priority: priority,
+    dueDate: dueDate,
+  });
+  if (response.status !== 201) throw Error("Failed to create task");
+  return response.data;
+};
+export const fetchTasks = async () => {
+  const response = await axios.get("http://localhost:3000/tasks");
+  if (response.status !== 200) throw Error("Failed to fetch tasks");
+  return response.data;
+};
+
+export const updateTask = async ({ id, title, priority, status, dueDate }) => {
+  const response = await axios.patch(`http://localhost:3000/tasks/${id}`, {
+    title: title,
+    status: status,
+    priority: priority,
+    dueDate: dueDate,
+  });
+  if (response.status !== 200) throw Error("Failed to update task");
+
+  return response.data;
+};
+
+export const deleteTask = async ({ id }) => {
+  const response = await axios.delete(`http://localhost:3000/tasks/${id}`);
+  if (response.status !== 200) throw Error("Failed to Delete task");
+  return response.status;
+};
