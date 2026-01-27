60ef8e2e917b27a856a4dd54c92a2438114f1aad
Krish Shah <kshah@trueigtech.com>
2026-01-27 16:02:27 +0530

    Refactor: using url from .env

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL_USERS;
+
 const loginUser = async (credentials) => {
-  const response = await axios.get(
-    "http://localhost:3000/users?email=" + credentials.email,
-  );
+  const response = await axios.get(`${API_URL}'?email='` + credentials.email);
   const users = response.data;
   if (users.length === 0 || users[0].password !== credentials.password) {
     throw new Error("Invalid Credentials");
