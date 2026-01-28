ddcdc84256f50c770570bd0f7c51e179a24f4bd1
Krish Shah <kshah@trueigtech.com>
2026-01-28 16:47:38 +0530

    fix: Added error Handling and defensive coding

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL_USERS;
 
+if (!API_URL) throw new Error("VITE_API_URL_USERS is not defined");
+
 const loginUser = async (credentials) => {
-  const response = await axios.get(`${API_URL}?email=` + credentials.email);
-  const users = response.data;
-  if (users.length === 0 || users[0].password !== credentials.password) {
+  const response = await axios.get(`${API_URL}?email=` + credentials?.email);
+  const users = response?.data;
+  if (users?.length === 0 || users?.[0]?.password !== credentials?.password) {
     throw new Error("Invalid Credentials");
   }
   return users[0];
