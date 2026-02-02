7f2715e232182b6f703d1fe6eccb184a25691065
Krish Shah <kshah@trueigtech.com>
2026-02-02 23:04:25 +0530

    configured git and created navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Authentication/context/authContext";
+import Modal from "../../common/Modal/Modal.jsx";
+import LoginForm from "../Authentication/components/LoginForm/component/LoginForm.jsx";
+import SignUpForm from "../Authentication/components/SignUpForm/component/SignUpForm.jsx";
+import UserDetailsForm from "../Authentication/components/UserDetailForm/component/userDetailForm.jsx";
+
+const Navbar = () => {
+  const [modalType, setModalType] = useState(null);
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const closeModal = () => setModalType(null);
+
+  const handleLogout = () => {
+    closeModal();
+    logout();
+    navigate("/");
+  };
+
+  const modalConfig = {
+    login: { title: "Login", component: <LoginForm onClose={closeModal} /> },
+    signup: {
+      title: "Create Account",
+      component: <SignUpForm onClose={closeModal} />,
+    },
+    userDetails: {
+      title: "User Details",
+      component: <UserDetailsForm onClose={closeModal} />,
+    },
+  };
+
+  const currentModal = modalConfig[modalType];
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="nav-text">
+          <h2>TaskFlow-Lite</h2>
+          {user && <span>Hi, {user.email}</span>}
+        </div>
+
+        <div className="navbar-buttons">
+          {user ? (
+            <>
+              <button
+                className="form-button"
+                onClick={() => setModalType("userDetails")}
+              >
+                User Details
+              </button>
+              <button className="form-button" onClick={handleLogout}>
+                LogOut
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => setModalType("login")}
+                className="form-button"
+              >
+                Login
+              </button>
+              <button
+                onClick={() => setModalType("signup")}
+                className="form-button"
+              >
+                Sign Up
+              </button>
+            </>
+          )}
+        </div>
+
+        <Modal
+          isOpen={!!modalType}
+          onClose={closeModal}
+          title={currentModal?.title || ""}
+        >
+          {currentModal?.component}
+        </Modal>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
