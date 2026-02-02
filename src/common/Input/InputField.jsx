866c2220727c8b027b19385ae1db6d8df314bb07
Krish Shah <kshah@trueigtech.com>
2026-02-02 23:08:26 +0530

    Created custom input component to render forms

--- src/common/Input/InputField.jsx
+++ src/common/Input/InputField.jsx
@@ -1,4 +1,4 @@
-import ReactDatePicker from "react-datepicker";
+import React from "react";
 import { Controller } from "react-hook-form";
 
 export default function InputField({
@@ -8,10 +8,85 @@ export default function InputField({
   placeholder,
   rules,
   type,
+  options,
 }) {
+  if (type === "radio") {
+    return (
+      <fieldset className="form-radio">
+        <legend>{label}</legend>
+        <Controller
+          control={control}
+          name={name}
+          rules={rules}
+          render={({ field }) => (
+            <div>
+              {options.map((optionValue) => (
+                <label key={optionValue}>
+                  <input
+                    type="radio"
+                    onChange={field.onChange}
+                    checked={field.value === optionValue}
+                    value={optionValue}
+                    className="form-radio"
+                  />
+                  {optionValue}
+                </label>
+              ))}
+            </div>
+          )}
+        />
+      </fieldset>
+    );
+  }
+
+  if (type === "checkbox") {
+    return (
+      <div className="form-checkbox">
+        <Controller
+          control={control}
+          name={name}
+          rules={rules}
+          render={({ field: { onChange, value, ref } }) => (
+            <input
+              type="checkbox"
+              id={name}
+              ref={ref}
+              onChange={(e) => onChange(e.target.checked)}
+              checked={!!value}
+              className="form-checkbox"
+            />
+          )}
+        />
+        <label htmlFor={name}>{label}</label>
+      </div>
+    );
+  }
+  if (type === "select") {
+    return (
+      <div className="form-select">
+        <label htmlFor={name}>{label}</label>
+        <Controller
+          control={control}
+          name={name}
+          rules={rules}
+          render={({ field }) => (
+            <select {...field} id={name} className="form-input">
+              <option value="">{placeholder || "Select option"}</option>
+              {options.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          )}
+        />
+      </div>
+    );
+  }
   return (
-    <div>
+    <div className="form-input-fields">
       <label htmlFor={name}> {label} </label>
+
       <Controller
         control={control}
         name={name}
@@ -26,6 +101,7 @@ export default function InputField({
             type={type}
             name={name}
             id={name}
+            className="form-input"
           />
         )}
       />
